140bd479d40d71d140fc7c5eb563c0f876670891
ryouze <98982999+ryouze@users.noreply.github.com>
2024-06-10 21:19:57 +0200

    [src] Add comments for include headers, add [[nodiscard]], use C++ types and casts, remove unnecessary code, use snake_case.

--- src/core/filepaths.cpp
+++ src/core/filepaths.cpp
@@ -4,18 +4,18 @@
 
 #include "filepaths.hpp"
 
+#include <cstdint>     // for std::uint32_t
 #include <filesystem>  // for std::filesystem
 #include <stdexcept>   // for std::runtime_error
 #include <string>      // for std::string
 #include <vector>      // for std::vector
 
 #if defined(_WIN32)
-#include <windows.h>
+#include <windows.h>  // for GetModuleFileNameA
 #elif defined(__APPLE__)
-#include <cstdint>
-#include <mach-o/dyld.h>
+#include <mach-o/dyld.h>  // for _NSGetExecutablePath
 #elif defined(__linux__)
-#include <unistd.h>
+#include <unistd.h>  // for readlink
 #endif
 
 namespace {
@@ -34,21 +34,20 @@ class PathError : public std::runtime_error {
  *
  * @return Path to the executable (e.g., "~/GitHub/shogun/build/shogun").
  */
-std::filesystem::path getExecutablePath()
+[[nodiscard]] std::filesystem::path get_executable_path()
 {
     std::vector<char> buffer(4096);  // Dynamically allocate buffer
 
 #if defined(_WIN32)
-    if (GetModuleFileNameA(NULL, buffer.data(), buffer.size()) == 0) {
+    if (GetModuleFileNameA(NULL, buffer.data(), static_cast<DWORD>(buffer.size())) == 0) {
         throw PathError("Failed to get the executable path");
     }
 #elif defined(__APPLE__)
-    uint32_t size = static_cast<uint32_t>(buffer.size());
+    std::uint32_t size = static_cast<std::uint32_t>(buffer.size());
     if (_NSGetExecutablePath(buffer.data(), &size) != 0) {
         throw PathError("Failed to get the executable path");
     }
-    const std::filesystem::path p(buffer.data());
-    return std::filesystem::canonical(p);
+    return std::filesystem::canonical(buffer.data());
 #elif defined(__linux__)
     const ssize_t count = readlink("/proc/self/exe", buffer.data(), buffer.size());
     if (count == -1) {
@@ -67,7 +66,7 @@ std::filesystem::path getExecutablePath()
  *
  * This variable is not accessible outside of this ".cpp" file.
  */
-const std::filesystem::path root = getExecutablePath().parent_path();
+const std::filesystem::path root = get_executable_path().parent_path();
 
 }  // namespace
 
