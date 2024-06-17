cdf545d5ad6a32c90d5d2b33a17023c025d4042f
ryouze <98982999+ryouze@users.noreply.github.com>
2024-06-17 23:53:02 +0200

    [all] Add my custom pathmaster library.

--- src/core/filepaths.cpp
+++ src/core/filepaths.cpp
@@ -4,73 +4,16 @@
 
 #include "filepaths.hpp"
 
-#include <cstdint>     // for std::uint32_t
-#include <filesystem>  // for std::filesystem
-#include <stdexcept>   // for std::runtime_error
-#include <string>      // for std::string
-#include <vector>      // for std::vector
-
-#if defined(_WIN32)
-#include <windows.h>  // for GetModuleFileNameA
-#elif defined(__APPLE__)
-#include <mach-o/dyld.h>  // for _NSGetExecutablePath
-#elif defined(__linux__)
-#include <unistd.h>  // for readlink
-#endif
+#include <pathmaster/pathmaster.hpp>
 
 namespace {
-
-/**
- * @brief Base class for exceptions raised during the retrieval of the executable path.
- */
-class ExecutablePathError : public std::runtime_error {
-  public:
-    explicit ExecutablePathError(const std::string &message)
-        : std::runtime_error("ExecutablePathError: " + message) {}
-};
-
-/**
- * @brief Helper function to get the path to the executable.
- *
- * @return Path to the executable (e.g., "~/GitHub/shogun/build/shogun").
- *
- * @throws ExecutablePathError If failed to get the executable path.
- *
- * @note This supports Windows, macOS, and Linux. Other platforms are not supported.
- */
-[[nodiscard]] std::filesystem::path get_executable_path()
-{
-    std::vector<char> buffer(4096);  // Dynamically allocate buffer
-
-#if defined(_WIN32)
-    if (GetModuleFileNameA(NULL, buffer.data(), static_cast<DWORD>(buffer.size())) == 0) {
-        throw ExecutablePathError("Failed to get the executable path");
-    }
-#elif defined(__APPLE__)
-    std::uint32_t size = static_cast<std::uint32_t>(buffer.size());
-    if (_NSGetExecutablePath(buffer.data(), &size) != 0) {
-        throw ExecutablePathError("Failed to get the executable path");
-    }
-    return std::filesystem::canonical(buffer.data());
-#elif defined(__linux__)
-    const ssize_t count = readlink("/proc/self/exe", buffer.data(), buffer.size());
-    if (count == -1) {
-        throw ExecutablePathError("Failed to get the executable path");
-    }
-    buffer[static_cast<std::vector<char>::size_type>(count)] = '\0';
-#else
-    throw ExecutablePathError("Failed to get the executable path, unsupported platform");
-#endif
-
-    return std::filesystem::canonical(buffer.data());
-}
 
 /**
  * @brief Helper variable containing the path to the directory containing the output executable (e.g., "~/GitHub/shogun/build").
  *
  * This variable is not accessible outside of this ".cpp" file.
  */
-const std::filesystem::path root = get_executable_path().parent_path();
+const std::filesystem::path root = pathmaster::get_executable_path().parent_path();
 
 }  // namespace
 
