beb2dc69285d179b64a18bf5d13ea7a41354cb27
ryouze <98982999+ryouze@users.noreply.github.com>
2024-07-06 03:38:19 +0200

    [src] Turn gradient into solid color to greatly improve performance.

--- src/app.cpp
+++ src/app.cpp
@@ -273,7 +273,6 @@ void app::run(
     });
 
     // Main renderer for the main application screen
-    const auto main_renderer_background = LinearGradient(Color::Pink1, Color::SkyBlue1);
     const auto main_renderer = Renderer([&] {
                                    // If loading, render the loading screen
                                    if (is_loading) {
@@ -334,7 +333,7 @@ void app::run(
                                           }) |
                                           border | bgcolor(Color::Grey11) | center;
                                }) |
-                               bgcolor(main_renderer_background) | color(Color::Pink1);
+                               bgcolor(Color::Pink1) | color(Color::Pink1);
 
     // Run the loading thread
     std::thread load_thread([&]() {
